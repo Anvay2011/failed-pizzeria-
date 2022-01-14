0909a2ee9d61b152a39252261117fcd3ced36944
Anvay2011 <83815773+Anvay2011@users.noreply.github.com>
2022-01-14 19:41:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+menu_list_array = ["Veg Margherita Pizza", "Extra Cheese", "hello"];
+
+function getmenu(){
+var htmldata="";
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+}
+function add_item(){
+var htmldata;
+var imgtags='<img id="img1" src="https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=2000&h=1000&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2021%2F06%2F15%2Fmozzarella-pizza-margherita-FT-RECIPE0621.jpg"/>';
+var item=document.getElementById("add_item").value;
+        menu_list_array.sort();
+        htmldata="";
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
